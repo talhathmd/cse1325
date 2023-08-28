705be8182acea3619361d612b3ee9b4a608af723
Talha Tahmid <talhathmd@gmail.com>
2023-08-28 16:37:12 -0500

    P01 extreme bonus

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,7 @@
+
+public class Hello {
+    public static void main(String[] args) {
+        String name = System.getProperty("user.name");
+        System.out.println("Hello, " + name + "!");
+    }
+}
