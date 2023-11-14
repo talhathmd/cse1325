67a4a73ec3862f5309600a5022a84ac2a1bbda17
Talha Tahmid <talhathmd@gmail.com>
2023-11-13 20:01:27 -0600

    P10 Full Credit

---
+++ P10/full_credit/Time.h
@@ -0,0 +1,34 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+
+class Time {
+private:
+    int _hour;
+    int _minute;
+    int _second;
+
+public:
+    Time(int hour, int minute, int second);
+    Time(); // Default constructor
+
+    Time operator+(const Time& time) const;
+    Time operator++(); // Pre-increment
+    Time operator++(int); // Post-increment
+    bool operator==(const Time& time) const;
+    bool operator!=(const Time& time) const;
+    bool operator<(const Time& time) const;
+    bool operator>(const Time& time) const;
+    bool operator<=(const Time& time) const;
+    bool operator>=(const Time& time) const;
+    friend std::ostream& operator<<(std::ostream& ost, const Time& time);
+    friend std::istream& operator>>(std::istream& ist, Time& time);
+
+
+private:
+    void rationalize();
+    int compare(const Time& time) const;
+};
+
+#endif // TIME_H
