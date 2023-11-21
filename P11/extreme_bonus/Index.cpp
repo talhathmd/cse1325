06d60cfb4637b7a06dfa44dd966a8512c0ffc7ea
Talha Tahmid <talhathmd@gmail.com>
2023-11-21 06:55:57 -0600

    Fixing ExtBonus

--- P11/extreme_bonus/Index.cpp
+++ P11/extreme_bonus/Index.cpp
@@ -6,23 +6,31 @@ void Index::add_word(const Word& word, const std::string& filename, int line) {
 }
 
 std::ostream& operator<<(std::ostream& ost, const Index& index) {
-    std::unordered_set<Index::Word> unique_words;
+    std::unordered_set<Index::Word> printed_words;
+
     for (const auto& entry : index._index) {
-        unique_words.insert(entry.first);
+        const auto& word = entry.first;
+        const auto& location = entry.second;
+
+        // Check if the word has already been printed
+        if (printed_words.find(word) == printed_words.end()) {
+            ost << word << ": " << location;
+
+            // Mark the word as printed
+            printed_words.insert(word);
+
+            // Find other occurrences of the same word and print them
+            auto range = index._index.equal_range(word);
+            auto it = range.first;
+            ++it; // Move to the next occurrence
+            while (it != range.second) {
+                ost << ", " << it->second;
+                ++it;
+            }
+
+            ost << std::endl;
+        }
     }
 
-    for (const auto& word : unique_words) {
-        ost << word << ": ";
-        auto range = index._index.equal_range(word);
-        auto it = range.first;
-        while (it != range.second) {
-            ost << it->second;
-            ++it;
-            if (it != range.second) {
-                ost << ", ";
-            }
-        }
-        ost << std::endl;
-    }
     return ost;
 }
